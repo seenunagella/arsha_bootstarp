4f8607c985511a4d429365ea056314c63a11be8c
seenunagella <srinuharshi12@gmail.com>
2024-10-21 10:20:56 +0530

    first commit

---
+++ bootstrap.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.your-class').slick({
+        
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 3000,
+          
+    });
+  });
+
+  $(document).ready(function(){
+        $('.slide').slick({
+            
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 2000,
+              
+        });
+      });
